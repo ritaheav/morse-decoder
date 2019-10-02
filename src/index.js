4af837ede082b70e446cf30a827a1cd46d2709aa
Маргарита Небесная <rita20071995@hotmail.com>
2019-10-02 10:32:39 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -36,11 +36,41 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
-function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
+const MORSE_SIGNALS = {
+    10: ".",
+    11: "-",
+    "**********": " ",
+  };
+  
+  function decode(expr) {
+    const LETTER_LENGTH = 10;
+    let decodedExpr = "";
+    const encWords = expr.split("**********");
+    encWords.forEach((v, i) => {
+      v.length % LETTER_LENGTH
+        ? (encWords[i] =
+            v + "0".repeat(LETTER_LENGTH - (v.length % LETTER_LENGTH)))
+        : "";
+    });
+    encWords.forEach((v, index) => {
+      let decodedString = "";
+      for (let i = 0; i < v.length; i += LETTER_LENGTH) {
+      const encLetter = v.slice(i, i + LETTER_LENGTH);
+        let morseLetter = "";
+        for (let j = 0; j < LETTER_LENGTH; j += 2) {
+            const encSign = encLetter.slice(j, j + 2);
+            morseLetter += MORSE_SIGNALS[encSign] ? MORSE_SIGNALS[encSign] : "";
+        }
+        decodedString += MORSE_TABLE[morseLetter];
+        decodedString +=
+          i === v.length - LETTER_LENGTH && index + 1 !== encWords.length
+            ? " "
+            : "";
+      }
+      decodedExpr += decodedString;
+    });
+    return decodedExpr;
+  }
+  module.exports = {
     decode
-}
+  };
